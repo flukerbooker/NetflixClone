a72b77753f8680096ce01314404e4920c8420398
flukerbooker <win.vorapol52@gmail.com>
2020-12-10 10:15:15 +0700

    [DELETED] Eslint bugged

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -7,10 +7,6 @@ export const Background = styled.div`
     background: url(${({src}) => (src ? `../images/misc/$(src).jpg` : '../images/misc/home-bg.jpg')}) top left / cover no-repeat;
 `;
 
-export const Frame = styled.div`
-
-`;
-
 export const Container = styled.div`
     display: flex;
     margin: 0 56px;
